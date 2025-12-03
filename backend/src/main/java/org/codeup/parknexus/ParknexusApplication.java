e2028906f20da242678c9cf3ae6b1a709640ae17
tonys-dev <santiagor.acarlos@gmail.com>
2025-12-03 10:07:05 -0500

    Feat/backend: update configuration files and add initial database schema for parking management system

---
+++ backend/src/main/java/org/codeup/parknexus/ParknexusApplication.java
@@ -0,0 +1,11 @@
+package org.codeup.parknexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParknexusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ParknexusApplication.class, args);
+    }
+}
